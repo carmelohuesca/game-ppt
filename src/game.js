89119bcfd31f9744fa81c51a789fc0392c65c3cf
Carmelo Vicente Huesca Calatayud <carmelovicente.huesca@aqualogybs.net>
2016-12-21 00:28:02 +0100

    responsabilidad unica en métodos

--- src/game.js
+++ src/game.js
@@ -1,8 +1,14 @@
+/**
+ * @namespace moduleGame
+ * @description modulo del juego Piedra-Papel-Tijera
+ * @return {object}  Objeto con la clase del juego
+ */
 var moduleGame = (function() {
     'use strict';
 
     /**
-     * @class Game
+     * @class moduleGame.Game
+     * @memberOf moduleGame
      * @description Representa el juego de Piedra-Papel-Tijera.
      * @constructor
      * @param {string} playerOne - Nombre del jugador 1.
@@ -13,45 +19,65 @@ var moduleGame = (function() {
         this.playerTwo = playerTwo;
         this.init();
     };
+
     /**
-     * @method init
-     * @memberOf Game
+     * method init
+     * memberOf Game
      * @description Método que inicializa las rondas.
      */
+
     Game.prototype.init = function() {
-        this.rounds = 0;
-        this.CHOICE = {
+        this.CHOICE = Object.freeze({
             ROCK: 'piedra',
             PAPER: 'papel',
             SCISSORS: 'tijeras'
-        };
-        this.RESULTS = {
+        });
+        this.RESULTS = Object.freeze({
             DRAW: 'empate',
             PLAYERONEWINS: 'gana el jugador ' + this.playerOne,
             PLAYERTWOWINS: 'gana el jugador ' + this.playerTwo
-        };
+        });
+        this.resetRound();
     };
 
     /**
-     * Método que se ejecuta en cada ronda.
-     * @method round
-     * @memberOf Game
+     * @description Método para reiniciar las rondas
+     * method resetRound
+     * memberOf Game
+     */
+    Game.prototype.resetRound = function() {
+        this.rounds = 0;
+    };
+
+    /**
+     * @description Método que se va a ejecutar cuando se cambie de ronda
+     * method changeRound
+     * memberOf Game
+     */
+    Game.prototype.changeRound = function() {
+        this.rounds++;
+    };
+
+    /**
+     * @description Método que se ejecuta en cada ronda.
+     * method round
+     * memberOf Game
      * @param  {string} playerOneChoice Elección del jugador 1.
      * @param  {string} playerTwoChoice Elección del jugador 2.
      * @return {string}                 Resultado de la ronda.
      */
     Game.prototype.round = function(playerOneChoice, playerTwoChoice) {
-        this.logic(playerOneChoice, playerTwoChoice);
-        this.rounds++;
-        return this.result;
+        this.changeRound();
+        return this.logic(playerOneChoice, playerTwoChoice);
     };
 
     /**
-     * Método que se encarga de ejecutar la lógica.
-     * @method logic
-     * @memberOf Game
+     * @description Método que se encarga de ejecutar la lógica.
+     * method logic
+     * memberOf Game
      * @param  {string} playerOneChoice Elección del jugador 1.
      * @param  {string} playerTwoChoice Elección del jugador 2.
+     * @return {string}                 Resultado de la ronda.
      */
     Game.prototype.logic = function(playerOneChoice, playerTwoChoice) {
         if (playerOneChoice === playerTwoChoice) {
@@ -69,11 +95,12 @@ var moduleGame = (function() {
                     break;
             }
         }
+        return this.result;
     };
 
     /**
-     * @method playerOneRockChoice
-     * @memberOf Game
+     * method playerOneRockChoice
+     * memberOf Game
      * @description Lógica cuando el jugador 1 elige Piedra.
      * @param  {string} playerTwoChoice Elección del jugador 2.
      */
